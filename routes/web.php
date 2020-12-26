8937980966119a569ecb3b2c427168e678edf8ed
Patricia Pires Dias <ppiresdias@umassd.edu>
2020-12-25 20:59:53 -0500

    install laravel-shopify

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('home');
+})->middleware(['auth.shopify'])->name('home');
