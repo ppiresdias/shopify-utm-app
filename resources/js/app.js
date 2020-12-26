21e0b672e2f16c5464e854dd5e629919e284d5a7
Patricia Pires Dias <ppiresdias@umassd.edu>
2020-12-25 22:05:27 -0500

    setup Polaris and AppBridge

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-require('./bootstrap');
+// require('./bootstrap');
+
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import {AppProvider, Page, Card} from '@shopify/polaris'
+import {Provider, TitleBar} from '@shopify/app-bridge-react'
+
+export default class App extends Component{
+
+    render(){
+
+        const config = {
+            apiKey : document.getElementById("apiKey").value,
+            shopOrigin : document.getElementById("shopOrigin").value,
+            forceRedirect : true
+        };
+
+        return (
+            <AppProvider>
+                <Provider config={config}>
+                    <TitleBar title="Polaris Demo" />
+                    <Page title="UTM Dashboard">
+                        <Card title="Online store dashboard" sectioned>
+                            <p>View a summary of your online store’s performance.</p>
+                        </Card>
+                    </Page>
+                </Provider>
+            </AppProvider>
+        );
+    }
+}
+
+if(document.getElementById('app')){
+    ReactDOM.render(<App />, document.getElementById('app'))
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <!-- You are: (shop domain name) -->
     <p>You are: {{ Auth::user()->name }}</p>
+    <div id="app"></div>
 @endsection
 
 @section('scripts')
